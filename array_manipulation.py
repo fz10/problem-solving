466ebf748bc1e6590d2478347d528fc0f1f7ed71
Felipe Zapata <f.zapata@utp.edu.co>
2021-04-26 16:19:47 -0500

    complexity reduced

--- array_manipulation.py
+++ array_manipulation.py
@@ -11,37 +11,31 @@ import sys
 def arrayManipulation(n, queries):
     array = [0 for x in range(n)]
     highest = 0
+    sum = 0
 
     for query in queries:
         start = query[0] - 1
-        end = query[1] - 1
+        end = query[1]
         num = query[2]
-        ind = start
-        for ind in range(start, end + 1):
-            array[ind] += num
-            if array[ind] > highest:
-                highest = array[ind]
+        array[start] += num
+        if (end < n):
+            array[end] -= num
+
+    for v in array:
+        sum += v
+        if sum > highest:
+            highest = sum
 
     return(highest)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-
     nm = input().split()
-
     n = int(nm[0])
-
     m = int(nm[1])
-
     queries = []
-
     for _ in range(m):
         queries.append(list(map(int, input().rstrip().split())))
-
-
     result = arrayManipulation(n, queries)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
